de0580996997c70daf4e6723792c8ee6d7df9c71
username <nomadicalli@mail.ru>
2022-01-23 12:04:23 +0300

    Задание 1

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const people = [ 
+    {name: 'Sandra', age: 18}, 
+    {name: 'Erin', age: 28}, 
+    {name: 'Carl', age: 42}, 
+    {name: 'Lloyd', age: 12}, 
+    {name: 'Samuel', age: 31}, 
+    {name: 'William', age: 65}, 
+    {name: 'Ric', age: 53}, 
+    {name: 'Mick', age: 12}, 
+    {name: 'Ludwig', age: 74},
+    {name: 'Hilaire', age: 10}, 
+    {name: 'Menachem', age: 4}, 
+    {name: 'Saul', age: 52}, 
+    {name: 'Robert', age: 65}, 
+    {name: 'Blair', age: 12}, 
+    {name: 'Robert', age: 81}, 
+    {name: 'Peter', age: 69}
+];
+
+people.forEach((item) => console.log(item))
